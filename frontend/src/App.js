ff57510ac8b333438c322256f35e84bee95812cf
DidulangaX <dilshandidulanga@gmail.com>
2023-03-10 21:31:02 +0530

    create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Lanka Flavor(PVT)Ltd.</a>
+      </header>
+      <main>List Product</main>
+    </div>
+  );
+}
+
+export default App;
